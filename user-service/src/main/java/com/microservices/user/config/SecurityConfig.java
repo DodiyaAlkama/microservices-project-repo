5c9b8cfcf6d65acbd81ced68cdcc810d2281d227
alkamaaixtor <alkama.dodiya@aixtor.com>
2026-08-18 18:24:52 +0530

    adding changes for properties and config file

--- user-service/src/main/java/com/microservices/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/microservices/user/config/SecurityConfig.java
@@ -34,11 +34,8 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/auth/**")
-                        .permitAll()
                         .anyRequest()
-                        .authenticated())
+                        .permitAll())
                 .build();
     }
 }
